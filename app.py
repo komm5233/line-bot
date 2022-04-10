2e81064e505c283013907814ec9f10790dd59032
Matt Chien <komm5233@hotmail.com>
2022-04-10 21:05:56 +0800

    Update

--- app.py
+++ app.py
@@ -39,12 +39,14 @@ def callback():
 def handle_message(event):
     msg = event.message.text
     r = '供沙小拉?'
-    if msg == 'hi':
+    if msg in ['hi', 'Hi']:
         r = 'hi'
     elif msg == '你吃飯沒':
         r = '還沒'
-    elif msg == '你在幹嘛':
+    elif msg in ['你在幹嘛', '在幹嘛']
         r = '在想尼^0^'
+    elif '快到了' in msg:
+        r = '那我下去幫你拿東西'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
